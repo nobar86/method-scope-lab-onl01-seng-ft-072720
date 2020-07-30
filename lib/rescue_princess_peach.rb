50639d22857325fa580c50f49cd541678375dd42
Michael Rabon <nobar86@gmail.com>
2020-07-30 02:10:52 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,9 +1,6 @@
-def rescue_princess_peach(phrase)
-  status = "rescued"
-  puts  "Hooray! Mario has rescued Princess Peach."
-end
 
 
 
- rescue_princess_peach
- puts status
+
+ #rescue_princess_peach
+ #puts status
